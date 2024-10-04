67818e982ba1a66e07d99adf05fc49b9a68c0caa
k4nkan <kanta.y.0126@gmail.com>
2024-10-04 23:45:21 +0900

    ランダム位置テキスト生成の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { useEffect, useState, useRef } from "react";
+import { getRundomPosition } from "@/component/getRundomPosition";
 
 export default function Main() {
   const [time, setTime] = useState(new Date());
   const [showColon, setShowColon] = useState(false);
   const [fontIndex, setFontIndex] = useState(0);
   const [isMouseMoving, setIsMouseMoving] = useState(false);
+  const [position, setPosition] = useState({x:0, y:0});
 
   const movementTimeout = useRef<number | null>(null); // useRefを使用してリファレンスを保持
 
@@ -46,7 +48,9 @@ export default function Main() {
       // 2秒間マウスが動かない場合、`isMouseMoving`をfalseに
       movementTimeout.current = window.setTimeout(() => {
         setIsMouseMoving(false);
-      }, 2000);
+        const {x,y} = getRundomPosition();
+        setPosition({x, y})
+      }, 1000);
     };
 
     // マウスムーブのリスナーを追加
@@ -63,19 +67,32 @@ export default function Main() {
     };
   }, []);
 
+  useEffect(() =>{
+    console.log(position);
+  },[position]);
+
   const hours = time.getHours().toString().padStart(2, "0");
   const minutes = time.getMinutes().toString().padStart(2, "0");
 
   return (
-    <div className="relative" onClick={handleClick} style={{ fontFamily: font_data[fontIndex] }}>
-      <div className="absolute flex flex-col">{isMouseMoving && <div>test</div>}
-      <div className="absolute flex items-center text-center justify-center text-5xl h-screen w-screen">
-        <div className="w-16">{hours}</div>
-        <div className="w-2 flex justify-center">
-          {showColon && <div>:</div>}
+    <div
+      className="relative"
+      onClick={handleClick}
+      style={{ fontFamily: font_data[fontIndex] }}
+    >
+      <div className="absolute flex flex-col">
+        {!isMouseMoving && (
+          <div style={{position: 'absolute', top: `${position.y}px`, left: `${position.x}px` }}>
+            <div>test</div>
+          </div>
+        )}
+        <div className="absolute flex items-center text-center justify-center text-5xl h-screen w-screen">
+          <div className="w-16">{hours}</div>
+          <div className="w-2 flex justify-center">
+            {showColon && <div>:</div>}
+          </div>
+          <div className="w-16">{minutes}</div>
         </div>
-        <div className="w-16">{minutes}</div>
-      </div>
       </div>
     </div>
   );
